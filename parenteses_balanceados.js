9b5032518175cbbf72e02af0dbb62d726671c949
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-19 16:19:59 -0300

    primeiro exercicio

---
+++ parenteses_balanceados.js
@@ -0,0 +1,22 @@
+/* Parênteses Balanceados
+Uma string que consiste em parênteses esquerdo e direito, "(" e ")", é dada e pede-se que você equilibre os parênteses inserindo parênteses conforme necessário. Determine o número mínimo de caracteres que devem ser inseridos.
+
+
+Descrição do formato da função solução
+1) Recebe como parâmetro uma string com parênteses.
+2) Retorne um número inteiro que deve ser o número mínimo de caracteres a serem adicionados na string.
+
+
+Exemplos
+Exemplo 1:
+Entrada: palavra="()))"
+Saída: 2
+
+Exemplo 2:
+Entrada: palavra="()()"
+Saída: 0
+
+Exemplo 3:
+Entrada: palavra="())("
+Saída: 2 */
+
